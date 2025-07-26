952ba9e44ed48e8a2316ca49e9e578150e6a5790
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-07-26 16:55:00 +0530

    init

---
+++ src/app.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { Logger } from "./config/logger";
+
+dotenv.config();
+const app = express();
+const logger = new Logger("app");
+const PORT = process.env.PORT || 8090;
+
+async function initServer() {
+  try {
+    app.use(express.json());
+    app.use(cors());
+
+    app.listen(PORT, () => {
+      logger.info(`PHANTOM APP SERVER LIVE ON PORT ${PORT}`);
+    });
+
+    logger.info("PHANTOM APP SERVER PROCESSES STARTED SUCCESSFULLY");
+  } catch (error) {
+    logger.error("PHANTOM APP SERVER PROCESSES STARTED FAILED DUE TO ", error);
+    process.exit(1);
+  }
+}
+
+process.on("SIGINT", () => {
+  logger.info("PHANTOM APP SERVER PROCESSES STOPPED SUCCESSFULLY");
+  process.exit(0);
+});
+
+process.on("SIGTERM", () => {
+  logger.info("Received SIGTERM. Gracefully shutting down...");
+  process.exit(0);
+});
+
+initServer().catch((error) => {
+  logger.error("Failed to start server:", error);
+  process.exit(1);
+});
