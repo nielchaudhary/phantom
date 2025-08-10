0e23bd50ec0aa6d31f71d144da964bb86544aa65
Neel Chaudhary <neilchaudhary12@gmail.com>
2025-08-10 12:29:03 +0530

    temp fix, use vite env later

--- app/src/lib/constants.ts
+++ app/src/lib/constants.ts
@@ -1,4 +1,1 @@
-export const PHANTOM_API_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8090"
-    : "https://phantom-18fu.onrender.com";
+export const PHANTOM_API_URL = "https://phantom-18fu.onrender.com";
